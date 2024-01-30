cfe7e02a216f6114d0fb35f193bb71d3e6b559f6
Vadim Semenov <vdm.semenov@gmail.com>
2024-01-30 15:03:56 +0600

    1111

--- js/main.js
+++ js/main.js
@@ -12,9 +12,7 @@ $(function(){
         $('.popup-container > input[type="text"]').val('');
     };
 
-    $('.order-call').on('click', function(){
-        showPopup();
-    });
+  
 
     $('.close').on('click', function(){
         clearForm();
